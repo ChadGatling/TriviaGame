ffb0cd411cc6ae8a1e637512fff220a155857147
Elliott Wendel <ewendel88@gmail.com>
2017-08-28 15:13:28 -0500

    click handler in start mode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,62 +3,73 @@ $(document).ready(function() {
     var playerName = "";
 
     loop();
-	
-	function loop() {
-	if (mode === "start") {// Start Screen-----------------------------------------------------------
-		console.log('mode === "start"');
-		    var startScreen =
-		        '<h1>Hangry Trivia</h1>' +
-		        '<div>' +
-		        '<h2><p>Hello ' + playerName + '. </p> Enter key or click to start.</h2>' +
-		        '</div>';
 
-	        getName();
+    function loop() {
+    	console.log('Mode === ' + mode);
+        if (mode === "start") { // Start Screen-----------------------------------------------------------
+            getName();
 
-	        $("body").html(startScreen);
-	        $("html").click(function() {
-	        	mode = "ask";
-	        	loop();
-	        })
+            var startScreen =
+                '<h1>Boozie Trivia</h1>' +
+                '<div>' +
+                '<h2><p>Hello ' + playerName + '. </p> Click to start.</h2>' +
+                '</div>';
+
+            $("body").html(startScreen);
+            $("html").click(function() {
+                mode = "ask";
+                loop();
+            })
+        } else if (mode === "ask") { // Question-------------------------------------------------------------
+            console.log('mode === ' + mode)
+            var randomOrder = 0;
+
+            var question = [
+                "When found on a beer bottle, what does the acronym IPA stand for?",
+                "To be legally sold as bourbon, a whiskey's mash must contain at least 51% of what grain?",
+            ];
+
+            var answer = [
+                ["India Pale Ale", "Irish Person Alcohol", "International Perscription Agitator", "Is Proper Alcohol"],
+                ["Corn", "Wheat", "Barly", "Sesame"]
+            ];
+
+            var questionNumber = Math.floor(Math.random() * question.length);
+
+            $("body").html(
+                '<h1>Boozie Trivia</h1>' +
+                '<div>' +
+                '<h2><p>' + question[questionNumber] + '</p></h2>' +
+                '</div>'
+            );
+            for (var i = 0; i < 4; i++) {
+            	$("body").append(
+            	'<div>' +
+                '<button type="button">' + answer[questionNumber][i] + '</button>' +
+                '</div>'
+            	)
+
+        	$("button").click(function() {
+        		console.log(this);
+        	});
+            }
+            // loop();
+        }
+
+        // Congratulations Screen----------------------------------------------------------------
+
+        // Wrong answer screen--------------------------------------------------------------
+
+        // Final Screen-------------------------------------------------------------------------
+
+	    function getName() {
+	        playerName = prompt("Please pick a name.");
+	        if (playerName === "") {
+	            console.log('case "":');
+	            playerName = prompt("Sorry you need to pick a name.");
+	            getName();
+	        }
 	    }
-
-	    else if (mode === "ask") {// Question-------------------------------------------------------------
-	    	console.log('mode === "ask"')	
-		    var askTrivia =
-		        '<h1>Hangry Trivia</h1>' +
-		        '<div>' +
-		        '<h2><p>' + question + '</p></h2>' +
-		        '</div>';
-		    var question = [
-		        "When found on a beer bottle, what does the acronym IPA stand for?",
-		    ];
-
-		    var answer = [
-		        "India Pale Ale",
-		    ];
-
-		    $("body").html(
-	        '<h1>Hangry Trivia</h1>' +
-	        '<div>' +
-	        '<h2><p>' + question[0] + '</p></h2>' +
-	        '</div>'
-		    	);
-		    loop();
-		}
-
-	    // Congratulations Screen----------------------------------------------------------------
-
-	    // Wrong answer screen--------------------------------------------------------------
-
-	    // Final Screen-------------------------------------------------------------------------
-	}
-});
-
-function getName() {
-	playerName = prompt("Please pick a name.");
-    if (playerName === "") {
-        console.log('case "":');
-        playerName = prompt("Sorry you need to pick a name.");
-        getName();
     }
-}
+
+});
